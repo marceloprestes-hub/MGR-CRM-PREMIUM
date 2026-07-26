3ea4fceed428729ad1b39e441dfc232bf20ad085
marceloprestes-hub <marceloprestes.economista@gmail.com>
2026-07-26 11:29:01 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const dashboard = {
+
+        leads: 0,
+        clientes: 0,
+        consultorias: 0,
+        conversao: 0
+
+    };
+
+    atualizarDashboard(dashboard);
+
+});
+
+function atualizarDashboard(dados){
+
+    document.getElementById("totalLeads").textContent = dados.leads;
+
+    document.getElementById("totalClientes").textContent = dados.clientes;
+
+    document.getElementById("totalConsultorias").textContent = dados.consultorias;
+
+    document.getElementById("taxaConversao").textContent = dados.conversao + "%";
+
+}
